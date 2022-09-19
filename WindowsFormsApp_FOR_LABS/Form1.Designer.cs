09567457f46c5161f650a25fda77888c7b5e5315
Dan'ka <den.krut02@mail.ru>
2022-09-19 20:25:55 +0300

    Часть 3

--- WindowsFormsApp_FOR_LABS/Form1.Designer.cs
+++ WindowsFormsApp_FOR_LABS/Form1.Designer.cs
@@ -28,64 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
-            // button1
+            // linkLabel1
             // 
-            this.button1.Location = new System.Drawing.Point(22, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 42);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Border Style";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(22, 60);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(116, 39);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Resize ";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(22, 105);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(116, 44);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Opacity";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(199, 139);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(56, 13);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Open form";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.ClientSize = new System.Drawing.Size(625, 365);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Location = new System.Drawing.Point(100, 200);
             this.Name = "Form1";
-            this.Opacity = 0.75D;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Trey Research";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- WindowsFormsApp_FOR_LABS/Form1.cs
+++ WindowsFormsApp_FOR_LABS/Form1.cs
@@ -37,5 +37,19 @@ namespace WindowsFormsApp_FOR_LABS
         {
             this.Opacity = 1;
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DialogResult aResult;
+            Form2 aForm = new Form2();
+            aResult = aForm.ShowDialog();
+            if (aResult == System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show("Your name is " + aForm.textBox1.Text + " " +
+                aForm.textBox2.Text);
+            }
+            linkLabel1.LinkVisited = true;
+
+        }
     }
 }
