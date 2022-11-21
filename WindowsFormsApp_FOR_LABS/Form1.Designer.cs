0830bfad04c4afaccc7f63352319339c06e2a9d4
Dan'ka <den.krut02@mail.ru>
2022-11-21 19:56:09 +0300

    1

--- WindowsFormsApp_FOR_LABS/Form1.Designer.cs
+++ WindowsFormsApp_FOR_LABS/Form1.Designer.cs
@@ -31,21 +31,28 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(24, 124);
+            this.textBox1.Location = new System.Drawing.Point(16, 89);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(537, 248);
+            this.textBox1.Size = new System.Drawing.Size(275, 248);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(590, 124);
+            this.button1.Location = new System.Drawing.Point(334, 90);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 45);
             this.button1.TabIndex = 1;
@@ -56,7 +63,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(590, 194);
+            this.button2.Location = new System.Drawing.Point(334, 160);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(133, 48);
             this.button2.TabIndex = 2;
@@ -64,9 +71,77 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(16, 343);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(518, 90);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(283, 247);
+            this.textBox2.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(518, 342);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(152, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(16, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(275, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(518, 56);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(283, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "IN file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(518, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "OUT file";
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(1016, 573);
+            this.ClientSize = new System.Drawing.Size(833, 380);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -88,6 +163,13 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WindowsFormsApp_FOR_LABS/Form1.cs
+++ WindowsFormsApp_FOR_LABS/Form1.cs
@@ -15,7 +15,25 @@ namespace WindowsFormsApp_FOR_LABS
         public Form1()
         {
             InitializeComponent();
+            comboBox1.Items.Add("ASCII");
+            comboBox1.Items.Add("Unicode");
+            comboBox1.Items.Add("UTF8");
+            comboBox1.Items.Add("UTF7");
+            comboBox1.Items.Add("UTF32");
 
+            Encoding ascii = Encoding.ASCII,
+                     unicode = Encoding.Unicode,
+                     utf8 = Encoding.UTF8,
+                     utf7 = Encoding.UTF7,
+                     utf32 = Encoding.UTF32;
+
+            comboBox2.Items.Add(ascii);
+            comboBox2.Items.Add(unicode);
+            comboBox2.Items.Add(utf8);
+            comboBox2.Items.Add(utf7);
+            comboBox2.Items.Add(utf32);
+
+            textBox3.Text = "Lab9.txt";
         }
 
 
@@ -28,29 +46,65 @@ namespace WindowsFormsApp_FOR_LABS
         private void button1_Click(object sender, EventArgs e)
         {
             // Создаем объекты двух кодировок.
-            Encoding ascii = Encoding.ASCII, unicode = Encoding.Unicode;
+            Encoding ascii   = Encoding.ASCII, 
+                     unicode = Encoding.Unicode,
+                     utf8    = Encoding.UTF8, 
+                     utf7    = Encoding.UTF7, 
+                     utf32   = Encoding.UTF32;
 
+            byte[] readStringBytes, writeStringBytes = { };
             using (StreamWriter writer = new StreamWriter("Lab9.txt", false))
             {
                 string text = textBox1.Text;
-                string textFile = "";
-                foreach (string s in text.Split('\r'))
+                Encoding temp = (System.Text.Encoding)comboBox2.SelectedItem;
+                char[] chTemp = { };
+                switch (comboBox1.SelectedItem.ToString())
                 {
-                    textFile += s;
-                }
-                // Перевод строки в массив байт (byte[]).
-                byte[] unicodeBytes = unicode.GetBytes(textFile);
+                    case "ASCII":
+                        readStringBytes = ascii.GetBytes(text);
+                        writeStringBytes = Encoding.Convert(ascii, temp, readStringBytes);
+                        chTemp = new char[temp.GetCharCount(writeStringBytes, 0, writeStringBytes.Length)];
 
-                byte[] asciiBytes = Encoding.Convert(unicode, ascii, unicodeBytes);
+                        break;
+                    case "Unicode":
+                        readStringBytes = unicode.GetBytes(text);
+                        writeStringBytes = Encoding.Convert(unicode, temp, readStringBytes);
+                        chTemp = new char[temp.GetCharCount(writeStringBytes, 0, writeStringBytes.Length)];
+                        break;
+                    case "UTF8":
+                        readStringBytes = utf8.GetBytes(text);
+                        writeStringBytes = Encoding.Convert(utf8, temp, readStringBytes);
+                        chTemp = new char[temp.GetCharCount(writeStringBytes, 0, writeStringBytes.Length)];
+                        break;
+                    case "UTF7":
+                        readStringBytes = utf7.GetBytes(text);
+                        writeStringBytes = Encoding.Convert(utf7, temp, readStringBytes);
+                        chTemp = new char[temp.GetCharCount(writeStringBytes, 0, writeStringBytes.Length)];
+                        break;
+                    case "UTF32":
+                        readStringBytes = utf32.GetBytes(text);
+                        writeStringBytes = Encoding.Convert(utf32, temp, readStringBytes);
+                        chTemp = new char[temp.GetCharCount(writeStringBytes, 0, writeStringBytes.Length)];
+                        break;
+                }
+                temp.GetChars(writeStringBytes, 0, writeStringBytes.Length, chTemp, 0);
+                string finalString = new string(chTemp);
+
+
+                //writeStringBytes = Encoding.Convert()
+
+                // Перевод строки в массив байт (byte[]).
+
+                //byte[] asciiBytes = Encoding.Convert(unicode, ascii, unicodeBytes);
 
                 // Конвертация массива байт в массив символов и строку.
                 // Пример использования GetCharCount/GetChars.
-                char[] asciiChars = new char[ascii.GetCharCount(asciiBytes, 0, asciiBytes.Length)];
-                ascii.GetChars(asciiBytes, 0, asciiBytes.Length, asciiChars, 0);
-                string asciiString = new string(asciiChars);
+                //char[] asciiChars = new char[ascii.GetCharCount(asciiBytes, 0, asciiBytes.Length)];
+                //ascii.GetChars(asciiBytes, 0, asciiBytes.Length, asciiChars, 0);
+                //string asciiString = new string(asciiChars);
 
 
-                writer.WriteLine(asciiString);
+                //writer.WriteLine(asciiString);
             }
         }
 
@@ -60,7 +114,35 @@ namespace WindowsFormsApp_FOR_LABS
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text != "" && comboBox1.GetItemText(comboBox1.SelectedIndex) != "-1")
+            {
+                
+                string path = textBox3.Text;
+                // асинхронное чтение
+                using (StreamReader reader = new StreamReader(new FileStream(path, FileMode.OpenOrCreate), (System.Text.Encoding)comboBox2.SelectedItem))
+                {
+                    textBox1.Text = await reader.ReadToEndAsync();                 
+                }
+            }
+            else
+                return;
+            
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
         }
